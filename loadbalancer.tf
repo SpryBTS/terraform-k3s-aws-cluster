a09111dcf042b737c0cf9cceddf8d2df0bfa9a60
Andrew Kent <andrew@sprybts.com>
2020-09-02 21:41:08 +1000

    trying this...

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -51,7 +51,7 @@ resource "aws_lb_listener" "port" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.agent[each.key].arn
+    target_group_arn = aws_lb_target_group.agent[${each.key}].arn
   }
 }
 
